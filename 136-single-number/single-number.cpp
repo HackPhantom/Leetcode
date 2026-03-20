b76c2904c4747a7488a02f88d35a43b53a38e38c
Jayant Kumar <jayant.kumar1065@gmail.com>
2026-03-20 23:11:49 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (82.98%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int num=0;
+        for(auto it: nums)
+        {
+            num^=it;
+        }
+        return num;
+        
+    }
+};
