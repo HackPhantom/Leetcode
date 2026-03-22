ddcd6c19c26eba3ab079fc1ec390a7ba98aba441
Jayant Kumar <jayant.kumar1065@gmail.com>
2026-03-22 13:06:48 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (48.46%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -4,19 +4,10 @@ public:
         vector<int> vec(3,0);
         for(auto i: nums) vec[i]++;
         int i=0,j=0;
-        for(i;i<vec[0];i++) 
-        {
-            nums[i]=0;
-        }
+        for(i;i<vec[0];i++) nums[i]=0;
         j=i;
-        for(i;i<j+vec[1];i++) 
-        {
-            nums[i]=1;
-        }
+        for(i;i<j+vec[1];i++) nums[i]=1;
         j=i;
-        for(i;i<j+vec[2];i++) 
-        {
-            nums[i]=2;
-        }
+        for(i;i<j+vec[2];i++) nums[i]=2;
     }
 };
