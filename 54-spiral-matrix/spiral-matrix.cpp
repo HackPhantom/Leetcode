680161d933d92849d628d263dcae3d917ae4e607
Jayant Kumar <jayant.kumar1065@gmail.com>
2026-04-03 11:28:51 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (14.40%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -10,34 +10,28 @@ public:
         int right = matrix[0].size() - 1;
         
         while (top <= bottom && left <= right) {
-            // 1. Traverse Right
             for (int j = left; j <= right; j++) {
                 ans.push_back(matrix[top][j]);
             }
-            top++; // Shrink top boundary
+            top++;
             
-            // 2. Traverse Down
             for (int i = top; i <= bottom; i++) {
                 ans.push_back(matrix[i][right]);
             }
-            right--; // Shrink right boundary
-            
-            // 3. Traverse Left
-            // We must check if the top boundary hasn't crossed the bottom
+            right--;
+
             if (top <= bottom) {
                 for (int j = right; j >= left; j--) {
                     ans.push_back(matrix[bottom][j]);
                 }
-                bottom--; // Shrink bottom boundary
+                bottom--;
             }
             
-            // 4. Traverse Up
-            // We must check if the left boundary hasn't crossed the right
             if (left <= right) {
                 for (int i = bottom; i >= top; i--) {
                     ans.push_back(matrix[i][left]);
                 }
-                left++; // Shrink left boundary
+                left++;
             }
         }
         
