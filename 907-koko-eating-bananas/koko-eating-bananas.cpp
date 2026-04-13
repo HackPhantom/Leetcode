64e2f6bf6366cf8dc482461d7397ea0169eec68a
Jayant Kumar <jayant.kumar1065@gmail.com>
2026-04-13 22:10:39 +0530

    Time: 8 ms (53.52%) | Memory: 22.8 MB (97.87%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+
+    bool fn(vector<int>& nums,int check,int h)
+    {
+        long sum = 0;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum += (nums[i]%check==0)?(nums[i]/check):(nums[i]/check+1);
+        }
+        if(sum<=h) return true;
+        return false;
+    }
+
+    int minEatingSpeed(vector<int>& nums, int h) {
+        int max = nums[0];
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i]>max) max = nums[i];
+        }
+
+        int ans = 0;
+        int low = 1, high = max; 
+        int mid;
+        
+        while(low<=high)
+        {
+            mid = low + (high-low)/2;
+            if (fn(nums,mid,h)) 
+            {
+                ans = mid;
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
+        return ans;
+
+    }
+};
