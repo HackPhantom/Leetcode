078fa002ee46e44a5d890dca6920cf269873a80b
Jayant Kumar <jayant.kumar1065@gmail.com>
2026-05-13 09:27:04 +0530

    Time: 190 ms (6.84%) | Memory: 9.6 MB (74.81%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        int check=0;
+        for(int i=0;i<s.size();i++)
+        {
+            check=0;
+            
+            for (int j=0;j<s.size();j++)
+            {
+                if (s[i] == t[j])
+                {
+                    s.erase(s.begin()+i);
+                    t.erase(t.begin()+j);
+                    check = 1;
+                    i--;
+                    break;
+                }
+            }
+            if (check==0) return false;
+        }
+        if (s==t && s=="") return true;
+        return false;
+        
+    }
+};
